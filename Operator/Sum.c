0ac83a975509694ca95e79aab363349721f55321
Bhim magar <shokokomichan098@gmail.com>
2025-05-18 01:09:06 +0545

    AllChange

--- Operator/Sum.c
+++ Operator/Sum.c
@@ -2,7 +2,16 @@
 int main() {
 
     int a,b,c;
+
+    // ask user to enter the first number
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    
+    // ask user to enter the second number
+    printf("Enter the second number:");
+    scanf("%d",&b);
+
     c =  a + b;
-    printf("%d",c);
+    printf("Sum is : %d\n",c);
     return 0;
 }
