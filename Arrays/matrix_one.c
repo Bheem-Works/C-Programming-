4537a76a2e4ec58635397962eef2075631b7d7ca
bheem-works <shokokomichan098@gmail.com>
2025-06-04 17:40:12 +0545

    matrix for the exam by the one loops

---
+++ Arrays/matrix_one.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[4][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+        {10, 11, 12}
+    };
+    int i, j;
+    
+    // Print matrix elements
+    printf("\nThe 4x3 matrix is:\n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
---
+++ Operator/SimpleInterest.c
@@ -0,0 +1,15 @@
+    #include <stdio.h> 
+
+    int main () {
+        // the varaibles name
+        float p,t;
+        p = 10000;
+        t=2;
+        float r = 0.05;
+
+        int simpleInterst  = p * t * r;
+
+        printf("%d",simpleInterst);
+
+        return 0;
+    }
